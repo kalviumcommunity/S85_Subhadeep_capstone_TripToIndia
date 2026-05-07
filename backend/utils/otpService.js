0dbe3dfd3867ac2c340c2d9912c94c5edaa49578
SUBHADEEP SAMANTA <140041464+Subhadeep-Samanta@users.noreply.github.com>
2026-05-07 15:02:21 +0530

    Change SMTP host to smtp-relay.brevo.com

--- backend/utils/otpService.js
+++ backend/utils/otpService.js
@@ -8,9 +8,9 @@ export const generateOTP = () => {
 
 // Create transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+   host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
   requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
