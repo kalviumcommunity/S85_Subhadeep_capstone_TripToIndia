7a6236f6453e6f94f19223f8942c554f47fa470a
SUBHADEEP SAMANTA <140041464+Subhadeep-Samanta@users.noreply.github.com>
2026-05-07 15:08:58 +0530

    Update otpService.js

--- backend/utils/otpService.js
+++ backend/utils/otpService.js
@@ -8,20 +8,13 @@ export const generateOTP = () => {
 
 // Create transporter
 const transporter = nodemailer.createTransport({
-   host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-  requireTLS: true,
+  host: "smtp-relay.brevo.com",
+  port: 2525,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 // Verify SMTP connection
